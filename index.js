022913b342cd49aa7483349ca85a9ee7cd3199fd
Akash <hafejmohammadakash@gmail.com>
2025-05-15 17:47:20 +0600

    ami je array off objects ta banaisi setake ekhon client site a dekhabo

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
 const app = express();
 const port = process.env.PROT || 5000;
 
@@ -7,19 +7,19 @@ const port = process.env.PROT || 5000;
 app.use(cors());
 
 const users = [
-  { name: "akash", email: "akash@gmail.com" },
-  { name: "fahim", email: "fahim@gmail.com" },
-  { name: "alif", email: "alif@gmail.com" },
-  { name: " nisat", email: "nisat@gmail.com" },
+  { id: 1, name: "akash", email: "akash@gmail.com" },
+  { id: 2, name: "fahim", email: "fahim@gmail.com" },
+  { id: 3, name: "alif", email: "alif@gmail.com" },
+  { id: 4, name: " nisat", email: "nisat@gmail.com" },
 ];
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/users", (rec, res) =>{
-  res.send(users)
-})
+app.get("/users", (rec, res) => {
+  res.send(users);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
