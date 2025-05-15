53ed450fe3688b4f9ce87266262ef9f459b14be0
Akash <hafejmohammadakash@gmail.com>
2025-05-15 19:19:54 +0600

    done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = process.env.PROT || 5000;
+const port = process.env.PORT || 5000;
 
 // medle ware
 app.use(cors());
