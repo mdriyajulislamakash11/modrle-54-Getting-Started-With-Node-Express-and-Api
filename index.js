5b6bfcc56c9ce8dcd248cad9854edb4bb1d146f2
Akash <hafejmohammadakash@gmail.com>
2025-05-15 17:25:01 +0600

    user create kore pathalam data base a

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const express = require('express')
-const app = express()
-const port = process.env.PROT || 5000
+const express = require("express");
+const app = express();
+const port = process.env.PROT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const users = [
+  { name: "akash", email: "akash@gmail.com" },
+  { name: "fahim", email: "fahim@gmail.com" },
+  { name: "alif", email: "alif@gmail.com" },
+  { name: " nisat", email: "nisat@gmail.com" },
+];
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/users", (rec, res) =>{
+  res.send(users)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
